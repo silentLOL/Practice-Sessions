b1e3ac18529e486810d5924342949b64c4d75d2a
Stefan Irndorfer <stefan.irndorfer@robart.cc>
2018-12-05 17:31:53 +0100

    Progress bar added, added Constants class, changed font size

--- app/src/main/java/at/stefanirndorfer/practicesessions/data/source/remote/RetrofitClient.java
+++ app/src/main/java/at/stefanirndorfer/practicesessions/data/source/remote/RetrofitClient.java
@@ -1,17 +1,18 @@
 package at.stefanirndorfer.practicesessions.data.source.remote;
 
+import at.stefanirndorfer.practicesessions.util.Constants;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://codingtest.fretello.com:3000";
+
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(Constants.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(new OkHttpClient())
                     .build();
---
+++ app/src/main/java/at/stefanirndorfer/practicesessions/util/Constants.java
@@ -0,0 +1,5 @@
+package at.stefanirndorfer.practicesessions.util;
+
+public class Constants {
+    public static final String BASE_URL = "http://codingtest.fretello.com:3000";
+}
